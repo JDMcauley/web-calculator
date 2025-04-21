fcd69d6e858c8167a60897da6da1d8a49fc736de
JDMcauley <123961794+JDMcauley@users.noreply.github.com>
2025-04-21 12:08:11 +0100

    calcualte multiple numbers before pressing equals

--- index.js
+++ index.js
@@ -1,93 +1,76 @@
-displayString = ""
-currentOperator = ""
-lastOperator = ""
-currentNum = ""
-lastNum = ""
-calculatedNum = ""
+let calcDisplay = ""
+let memDisplay = ""
+let currentOperator = ''
+let lastOperator = ''
+let isChain = false
+let calcualtedNum = 0
+let newChain = false
 
-function buttonClicked(num){
-    currentNum = currentNum + num
-    displayString = displayString.concat(num)
-    document.getElementById("calcDisplay").innerHTML = currentOperator.concat(displayString)
-}
-
-function clearDisplay(){
-    document.getElementById("calcDisplay").innerHTML = " "
-    displayString = ""
-    document.getElementById("memDisplay").innerHTML = " "
-    memString = ""
-    currentOperator = ""
-    lastOperator = ""
-    currentNum = ""
-    lastNum = ""
-}
-
-function addPressed(){
-    lastNum = currentNum
-    document.getElementById("memDisplay").innerHTML = lastNum
-    document.getElementById("calcDisplay").innerHTML = "+ "
-    displayString = ""
-    currentNum = ""
-    lastOperator = currentOperator
-    currentOperator = "+"
-}
-
-
-function minusPressed(){
-    lastNum = currentNum
-    document.getElementById("memDisplay").innerHTML = lastNum
-    document.getElementById("calcDisplay").innerHTML = "- "
-    displayString = ""
-    currentNum = ""
-    lastOperator = currentOperator
-    currentOperator = "-"
-}
-
-
-function multiplyPressed(){
-    lastNum = currentNum
-    document.getElementById("memDisplay").innerHTML = lastNum
-    document.getElementById("calcDisplay").innerHTML = "* "
-    displayString = ""
-    currentNum = ""
-    lastOperator = currentOperator
-    currentOperator = "*"
-}
-
-
-function dividePressed(){
-    lastNum = currentNum
-    document.getElementById("memDisplay").innerHTML = lastNum
-    document.getElementById("calcDisplay").innerHTML = "/ "
-    displayString = ""
-    currentNum = ""
-    lastOperator = currentOperator
-    currentOperator = "/"
-}
-
-function equalsPressed(){
-    if (lastNum !== ""){
-        switch(currentOperator){
-            case "+":
-                document.getElementById("memDisplay").innerHTML = currentNum.concat(" + ".concat(lastNum))
-                currentNum = Number(currentNum) + Number(lastNum)
-                document.getElementById("calcDisplay").innerHTML = "= ".concat(currentNum)
-                break;
-            case "-":
-                document.getElementById("memDisplay").innerHTML = currentNum.concat(" - ".concat(lastNum))
-                currentNum = Number(lastNum) - Number(currentNum)
-                document.getElementById("calcDisplay").innerHTML = "= ".concat(currentNum)
-                break;
-            case "*":
-                document.getElementById("memDisplay").innerHTML = currentNum.concat(" * ".concat(lastNum))
-                currentNum = Number(lastNum) * Number(currentNum)
-                document.getElementById("calcDisplay").innerHTML = "= ".concat(currentNum)
-                break;
-            case "/":
-                document.getElementById("memDisplay").innerHTML = currentNum.concat(" / ".concat(lastNum))
-                currentNum = Number(lastNum) / Number(currentNum)
-                document.getElementById("calcDisplay").innerHTML = "= ".concat(currentNum)
-                break;
-        }
+function buttonClicked(value){
+    if (newChain === true) {
+        calcDisplay = value
+        newChain = false
+    } else {
+        calcDisplay = calcDisplay.concat(value)
     }
+    document.getElementById("calcDisplay").innerHTML = currentOperator.concat(calcDisplay)
+}
+
+function clearDisplay() {
+    calcDisplay = ""
+    memDisplay = ""
+    currentOperator = ''
+    lastOperator = ''
+    calcualtedNum = 0
+    isChain = false
+    document.getElementById("calcDisplay").innerHTML = ""
+    document.getElementById("memDisplay").innerHTML = ""
+}
+
+function calculate() {
+    const num1 = Number(memDisplay)
+    const num2 = Number(calcDisplay)
+    switch (lastOperator) {
+        case '+':
+            calcualtedNum = num1 + num2
+            break
+        case '-':
+            calcualtedNum = num1 - num2
+            break
+        case '*':
+            calcualtedNum = num1 * num2
+            break
+        case '/':
+            calcualtedNum = num1 / num2
+            break
+    }
+}
+
+function calculateNumber(operator) { 
+    if (operator === '=') {
+        if (memDisplay !== "" && calcDisplay !== ""){
+            calculate()
+            document.getElementById("calcDisplay").innerHTML = "= ".concat(calcualtedNum)
+            document.getElementById("memDisplay").innerHTML = memDisplay.concat(lastOperator.concat(calcDisplay))
+            calcDisplay = String(calcualtedNum)
+            memDisplay = ""
+            currentOperator = ""
+            lastOperator = ""
+            isChain = false
+            newChain = true
+        }
+        return
+    }
+    if (isChain === true && calcDisplay !== "") {
+        calculate()
+        memDisplay = String(calcualtedNum)
+    } else if (isChain === false && calcDisplay !== ""){
+        memDisplay = calcDisplay
+    }
+    calcDisplay = ""
+    lastOperator = operator
+    currentOperator = operator
+    isChain = true
+    document.getElementById("memDisplay").innerHTML = memDisplay
+    document.getElementById("calcDisplay").innerHTML = currentOperator
 }
